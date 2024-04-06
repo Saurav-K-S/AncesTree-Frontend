2392e3ebb25ba27ea85305dcf807ae342be5e219
Saurav-K-S <sauravks002@gmail.com>
2024-04-06 11:47:41 +0530

    Enthokkeyo

--- AncesTree-Frontend/src/test.jsx
+++ AncesTree-Frontend/src/test.jsx
@@ -1,46 +1,67 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
-
+gsap.registerPlugin(useGSAP);
 export default function TEST() {
-  const boxRef = useRef(null);
-  const oneRef = useRef(null);
-  const twoRef = useRef(null);
-  const threeRef = useRef(null);
-  const fourRef = useRef(null);
-  
-  useEffect(() => {
-    const rotateAnimation = (trigger, rotation) => {
-      gsap.to(boxRef.current, {
-        rotation: rotation,
-        scrollTrigger: {
-          trigger: trigger,
-          start: "top top",
-          end: "bottom top",
-          scrub: true,
-        },
-        duration: 2,
-      });
-    };
+  const boxRef = useRef();
+  const oneRef = useRef();
+  const twoRef = useRef();
+  const threeRef = useRef();
+  const fourRef = useRef();
 
-    rotateAnimation(oneRef.current, 90);
-    rotateAnimation(twoRef.current, 180);
-    rotateAnimation(threeRef.current, 270);
-    rotateAnimation(fourRef.current, 360);
+  useGSAP(() => {
+    // const rotateAnimation = (trigger, rotation) => {
+      gsap.to(boxRef.current, {
+        rotation: 90,
+        scrollTrigger: {
+          trigger: oneRef.current,
+          start: "top center",
+          end: "bottom center",
+          scrub: true,
+          markers: true,
+        },
+      });
+    // };
+    gsap.config({ log: true });
+
+    // rotateAnimation(oneRef, 90);
+    // rotateAnimation(twoRef, 180);
+    // rotateAnimation(threeRef, 270);
+    // rotateAnimation(fourRef, 360);
   }, []);
+  // useEffect(() => {
+  //   const rotateAnimation = (trigger, rotation) => {
+  //     gsap.to(boxRef.current, {
+  //       x: rotation,
+  //       scrollTrigger: {
+  //         trigger: trigger,
+  //         start: "top top",
+  //         end: "bottom top",
+  //         scrub: true,
+  //       },
+  //       duration: 2,
+  //     });
+  //   };
+
+  //   rotateAnimation(oneRef.current, 90);
+  //   rotateAnimation(twoRef.current, 180);
+  //   rotateAnimation(threeRef.current, 270);
+  //   rotateAnimation(fourRef.current, 360);
+  // }, []);
 
   return (
-    <div className="h-screen w-screen overflow-scroll overflow-x-hidden scrollbar-thin">
+    <div className="h-screen w-screen   overflow-y-scroll scrollbar-thin">
       <div
         ref={boxRef}
-        className="box top-96 left-96 fixed h-14 w-14 bg-black"
+        className="box w-14 h-14 absolute bg-black"
       ></div>
-      <div ref={oneRef} className="one h-screen w-full bg-white"></div>
-      <div ref={twoRef} className="two h-screen w-full bg-slate-600"></div>
-      <div ref={threeRef} className="three h-screen w-full bg-red-600"></div>
-      <div ref={fourRef} className="four h-screen w-full bg-yellow-600"></div>
+      <div ref={oneRef} className="one h-full w-full bg-white"></div>
+      <div ref={twoRef} className="two h-full w-full bg-slate-600"></div>
+      <div ref={threeRef} className="three h-full w-full bg-red-600"></div>
+      <div ref={fourRef} className="four h-full w-full bg-yellow-600"></div>
     </div>
   );
 }
